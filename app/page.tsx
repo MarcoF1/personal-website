30bfee75c1de84a9312cdfc0786723919956cb49
MarcoF1 <marco1fleming@gmail.com>
2024-10-02 19:37:57 -0400

    removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Mail, Github, Linkedin, Globe } from "lucide-react";
+import { Mail, Github, Linkedin } from "lucide-react";
 
 export default function Page() {
   return (
